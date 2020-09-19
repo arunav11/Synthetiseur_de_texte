05bb0da612002cb473944f6ec449b9c6814bca13
Deepak Tiwari <tiwarid99@gmail.com>
2020-09-19 13:35:05 +0530

    Fixed mozilla and chrome issues

--- upload/forms.py
+++ upload/forms.py
@@ -10,7 +10,7 @@ class MediaFileForm(forms.ModelForm):
         self.label_suffix = ""
         self.fields['file'].widget.attrs['accept'] = ".wav,.mp4"
         self.fields['file'].widget.attrs['class'] = 'form-control-file'
-        self.fields['file'].widget.attrs['style'] = 'text-align: center'
+        self.fields['file'].widget.attrs['style'] = 'text-align: center; width: fit-content; display: -webkit-inline-box;'
 
     class Meta:
         model = MediaFile
